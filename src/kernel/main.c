c5716c4b21d293abb0c90158318fb8166869ceb8
baiyuwei123 <162284484+baiyuwei123@users.noreply.github.com>
2024-11-24 19:18:09 +0800

    1. 修改readme，增加页表构建的说明 2. 找到不能进kernel的原因，页表构建错误

---
+++ src/kernel/main.c
@@ -0,0 +1,19 @@
+#include "print.h"
+
+int main() {
+    put_char('k');
+    put_char('k');
+    put_char('k');
+    put_char('k');
+    put_char('k');
+    put_char('k');
+    put_char('k');
+    put_char('k');
+    put_char('k');
+    put_char('k');
+    put_char('k');
+    put_char('k');
+    put_char('k');
+    while(1);
+    return 0;
+}
---
+++ src/kernel/print.h
@@ -0,0 +1,5 @@
+#ifndef PRINT_H
+#define PRINT_H
+#include "stdint.h"
+void put_char(uint8_t char_asci);
+#endif
